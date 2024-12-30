535274ccbe713b8bf65c5a52d52b1a45c01eebc3
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2024-12-30 11:55:33 +0530

    migrating the entire demo project to use the context API

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,25 +1,21 @@
 import { CartContext } from "../store/shopping-cart-context";
-
-export default function Cart({ onUpdateItemQuantity }) {
-    // const {items} = useContext(CartContext);
+import { useContext } from "react";
+export default function Cart() {
+    const {items,updatedItemQuantity} = useContext(CartContext);
   
   
-
-
-  return (
-    <CartContext.Consumer>
-      {(cartCtx)=>{
-        const totalPrice = cartCtx.items.reduce(
+  const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
   const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
-        return(
-          <div id="cart">
-      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
-      {cartCtx.items.length > 0 && (
+
+  return (
+    <div id="cart">
+      {items.length === 0 && <p>No items in cart!</p>}
+      {items.length > 0 && (
         <ul id="cart-items">
-          {cartCtx.items.map((item) => {
+          {items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
@@ -29,11 +25,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
@@ -46,9 +42,5 @@ export default function Cart({ onUpdateItemQuantity }) {
         Cart Total: <strong>{formattedTotalPrice}</strong>
       </p>
     </div>
-
-        )
-      }}
-    </CartContext.Consumer>
-  )
+  );
 }
